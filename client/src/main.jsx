a48641cb1656ce400fd9e7e86cfd08ae218e9b55
grerm1996 <theclickypen@gmail.com>
2024-07-08 15:29:15 -0700

    fix service worker url?

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("/messaging-app/sw.js")
       .then((registration) => {
         console.log(
           "Service Worker registered successfully:",
